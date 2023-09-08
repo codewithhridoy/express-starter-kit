7b5410b1d2bb332955435df55ede04042dc74e58
hridoy.dev <hridoyahmed11197@gmail.com>
2023-09-08 17:42:42 +0600

    chore: initial setup & health check

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+require("dotenv").config();
+import express, { Request, Response } from "express";
+import morgan from "morgan";
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+app.get("/api/health-checker", (req: Request, res: Response) => {
+  res.status(200).json({
+    status: "success",
+    message: "✅ Starter Kit is working fine!",
+  });
+});
+
+app.all("*", (req: Request, res: Response) => {
+  res.status(404).json({
+    status: "fail",
+    message: `Route: ${req.originalUrl} does not exist on this server`,
+  });
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, async () => {
+  console.log("🚀Server started Successfully");
+});
